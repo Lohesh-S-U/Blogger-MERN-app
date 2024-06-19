8a7a0b2927fb5d8b1932d097493d665218cb111d
Lohesh S U <sulohesh@gmail.com>
2024-06-19 15:46:15 +0530

    feat:Auth API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors =  require('cors')
 const express=require('express')
 const mongoose=require('mongoose')
 const postRoutes = require('./routes/posts')
+const userRoutes = require('./routes/user')
 
 const app=express()
 
@@ -17,6 +18,7 @@ app.use((req,res,next)=>{
 })
 
 app.use('/api/posts',postRoutes)
+app.use('/api/user',userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
